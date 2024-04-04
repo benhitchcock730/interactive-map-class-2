9df1eeb96b8ef7c758a750583068cf4e2ee62bfd
benhitchcock730 <blh9133@nyu.edu>
2024-04-04 14:36:16 -0400

    Commit to reflect work ahead of 4/4 class

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-This is the first 1-2 week project with html, css, js basics
+Sometimes you go to the corner store to get yourself a little treat, but get to the front of the line and feel paralyzed by indecision. This tool isn't likely to actually help you out, but the colors are pretty at least.
+This is the project for weeks 1-3 of Interactive Web Mapping, using (very) basic html, css, and java. 
--- scripts.js
+++ scripts.js
@@ -5,16 +5,11 @@ $('.card1').on('click', function () {
 
 if (artist === 'reese') {
     $('#column-right').css('background-image', 'url("https://allcitycandy.com/cdn/shop/products/Screenshot2022-05-03121457_2048x.jpg?v=1651854655")')
-}
-})
-
-$('.card3').on('click', function () {
-    var artist = $(this).data('artist')
-
-    $('#column-right').empty()
-   
-    if (artist === 'peanut') {
-    $('#column-right').css('background-image', 'url("https://m.media-amazon.com/images/I/51gPmq+HigL._QL92_SH45_SS200_.jpg")')
+    $('body').css('background-color', '#fe5200')
+    $('.card1').css('font-style', 'italic') 
+    $('.card2').css('font-style', 'normal')
+    $('.card3').css('font-style', 'normal')
+    // I created a sort of active state by doing this but not sure if it's the most efficient way
 }
 })
 
@@ -26,21 +21,27 @@ $('.card2').on('click', function () {
    
     if (artist === 'crunch') {
     $('#column-right').css('background-image', 'url("https://m.media-amazon.com/images/I/61WL+Wx3J4L._AC_.jpg")')
+    $('body').css('background-color', 'hsl(208, 100%, 55%)')
+    $('.card1').css('font-style', 'normal')
+    $('.card2').css('font-style', 'italic')
+    $('.card3').css('font-style', 'normal') 
+}
+})
+
+
+$('.card3').on('click', function () {
+    var artist = $(this).data('artist')
+
+    $('#column-right').empty()
+   
+    if (artist === 'peanut') {
+    $('#column-right').css('background-image', 'url("https://m.media-amazon.com/images/I/51gPmq+HigL._QL92_SH45_SS200_.jpg")')
+    $('body').css('background-color', '#FDD835')
+    $('.card1').css('font-style', 'normal')
+    $('.card2').css('font-style', 'normal')
+    $('.card3').css('font-style', 'italic')
 }
 })
 
 
 
-
-
-$('.card3').on('click', function() {
-$('body').css('background-color', '#FDD835')
-})
-
-$('.card2').on('click', function() {
-$('body').css('background-color', '#1a1aff')
-})
-
-$('.card1').on('click', function() {
-$('body').css('background-color', '#fe5200')
-})
